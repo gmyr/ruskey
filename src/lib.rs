c431bc7da71feb96c7cfbd59239fae740c1bab05
Robert Gmyr <robert@gmyr.net>
2020-11-29 11:24:46 -0800

    REPL!

--- src/lib.rs
+++ src/lib.rs
@@ -277,3 +277,31 @@ pub mod lexer {
         }
     }
 }
+
+pub mod repl {
+    use super::lexer::Lexer;
+    use super::lexer::Token;
+    use std::io;
+    use std::io::Write;
+
+    pub fn start() {
+        loop {
+            print!(">> ");
+            io::stdout().flush().unwrap();
+
+            let mut input = String::new();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("failed to read line");
+
+            let mut lexer = Lexer::new(&input);
+            loop {
+                let token = lexer.next_token();
+                if token == Token::EOF {
+                    break;
+                }
+                println!("{:?}", token);
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("Hello, world!");
+    ruskey::repl::start();
 }
