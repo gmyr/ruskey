cd71de4abc3822d0ede03313da2f40f6b44e928d
Robert <robert@gmyr.net>
2020-11-30 23:20:37 -0800

    Impl Display trait for AST nodes

--- src/ast.rs
+++ src/ast.rs
@@ -1,7 +1,7 @@
 use std::any::Any;
+use std::fmt;
 
-// TODO: Do we actually need this?
-pub trait Node {}
+pub trait Node: fmt::Display {}
 
 pub trait Statement: Node {
     fn as_any(&self) -> &dyn Any;
@@ -13,10 +13,25 @@ pub struct Program {
     pub statements: Vec<Box<dyn Statement>>,
 }
 
+impl fmt::Display for Program {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for stmt in &self.statements {
+            write!(f, "{}", stmt)?;
+        }
+        Ok(())
+    }
+}
+
 impl Node for Program {}
 
 pub struct Identifier {
     pub value: String,
+}
+
+impl fmt::Display for Identifier {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.value)
+    }
 }
 
 impl Node for Identifier {}
@@ -26,6 +41,13 @@ impl Expression for Identifier {}
 pub struct LetStatement {
     pub name: Box<Identifier>,
     // pub value: Box<dyn Expression>,
+}
+
+impl fmt::Display for LetStatement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // TODO: add expression
+        write!(f, "let {} = ...;", self.name)
+    }
 }
 
 impl Node for LetStatement {}
@@ -40,10 +62,55 @@ pub struct ReturnStatement {
     // pub value: Box<dyn Expression>,
 }
 
+impl fmt::Display for ReturnStatement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // TODO: add expression
+        write!(f, "return ...;")
+    }
+}
+
 impl Node for ReturnStatement {}
 
 impl Statement for ReturnStatement {
     fn as_any(&self) -> &dyn Any {
         self
+    }
+}
+
+pub struct ExpressionStatement {
+    // expression: Box<dyn Expression>,
+}
+
+impl fmt::Display for ExpressionStatement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // TODO: add expression
+        write!(f, "")
+    }
+}
+
+impl Node for ExpressionStatement {}
+
+impl Statement for ExpressionStatement {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_display() {
+        let program = Program {
+            statements: vec![Box::new(LetStatement {
+                name: Box::new(Identifier {
+                    value: String::from("myVar"),
+                }),
+            })],
+        };
+
+        let a = format!("{}", program);
+        // TODO: update once expressions are implemented
+        assert_eq!(a, "let myVar = ...;");
     }
 }
